91151f6e0220d4317352bd380d9aa2cd3e754f42
GreatJun <ljun970@naver.com>
2023-11-28 17:45:12 +0900

    StartScene Character-Choice Image Add

---
+++ Assets/Scripts/StartScene/CharacterImageChange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterImageChange : MonoBehaviour
+{
+    [SerializeField] private Sprite changeImage_Knight;
+    [SerializeField] private Sprite changeImage_Wizzard;
+
+    private Image isImage;
+
+    private void Awake()
+    {
+        isImage = GetComponent<Image>();
+    }
+
+    public void Select_Knight()
+    {
+        isImage.sprite = changeImage_Knight;
+    }
+
+    public void Select_Wizzard()
+    {
+        isImage.sprite = changeImage_Wizzard;
+    }
+}
