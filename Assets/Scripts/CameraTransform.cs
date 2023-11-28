c63b54870ec046b92e9072ecb1a7d37cb4f15988
GreatJun <ljun970@naver.com>
2023-11-28 11:10:11 +0900

    Camera Follow Update

--- Assets/Scripts/CameraTransform.cs
+++ Assets/Scripts/CameraTransform.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class CameraTransform : MonoBehaviour
 {
-    public float cameraSpeed = 5.0f;
     [SerializeField] private GameObject player;
 
-    private void Awake()
+
+    private void Start()
     {
-        if(GameManager.instance.CharacterCheck() == 0)
-        {
-            player = Resources.Load<GameObject>("Prefabs/PlayerKnight");
-        }
-        else
-        {
-            if(GameManager.instance.CharacterCheck() == 1) player = Resources.Load<GameObject>("Prefabs/PlayerWizzard");
-        }
+        //if(GameManager.instance.CharacterCheck() == 0)
+        //{
+        //    player = Resources.Load<GameObject>("Prefabs/PlayerKnight");
+        //}
+        //else
+        //{
+        //    if(GameManager.instance.CharacterCheck() == 1) player = Resources.Load<GameObject>("Prefabs/PlayerWizzard");
+        //}
+        player = GameObject.FindWithTag("Player");
     }
 
-    public void Upadate()
+    public void LateUpdate()
     {
-        Vector3 dir = player.transform.position - this.transform.position;
-        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
-        this.transform.Translate(moveVector);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
     }
 }
