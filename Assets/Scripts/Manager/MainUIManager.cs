b8218db6abc416f64b008299b56e947f257b02e8
GreatJun <ljun970@naver.com>
2023-11-29 00:20:34 +0900

    Update

--- Assets/Scripts/Manager/MainUIManager.cs
+++ Assets/Scripts/Manager/MainUIManager.cs
@@ -7,6 +7,7 @@ public class MainUIManager : MonoBehaviour
 {
     public Text personUI;
 
+    // 파괴된 캐릭터의 정보
     GameObject beforeCharacter;
 
     [SerializeField] private GameObject player;
@@ -49,19 +50,23 @@ public class MainUIManager : MonoBehaviour
     }
 
     // 캐릭터 재선택
+    // 기사 선택
     public void KnightSelect()
     {
         GameManager.instance.KnightChoice();
     }
 
+    // 위자드 선택
     public void WizzardSelect()
     {
         GameManager.instance.WizzardChoice();
     }
 
+    // 고른 캐릭터 소환
     public void SelectCharacter()
     {
         GameObject player = GameManager.instance.InvokeCharacter();
+        // 새로 고른 캐릭터의 위치를 이전 파괴된 오브젝트의 위치로
         player.transform.position = beforeCharacter.transform.position;
         Invoke("PlayerNameSetting", 0.1f);
     }
